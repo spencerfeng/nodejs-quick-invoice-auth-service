1733e3e75aaf19b40264066245293b3bd7ef7cc5
Spencer <spencer@sana.com.au>
2020-04-24 19:37:10 +1000

    test: fix e2e tests for AppController which causes TypeORM connection issues

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,7 +6,7 @@ import { AppModule } from './../src/app.module'
 describe('AppController (e2e)', () => {
   let app: INestApplication
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule]
     }).compile()
@@ -38,5 +38,9 @@ describe('AppController (e2e)', () => {
           })
         })
     })
+  })
+
+  afterAll(async () => {
+    await app.close()
   })
 })
