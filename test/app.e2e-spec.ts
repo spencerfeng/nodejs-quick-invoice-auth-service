ccbedf35ab6a45588843db97140e57c3c0105d7d
Spencer <spencer@sana.com.au>
2020-04-26 17:04:09 +1000

    test: fix e2e tests after using TypeORM for users

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,35 +1,85 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import * as request from 'supertest'
 import { AppModule } from './../src/app.module'
+import { UsersModule } from '../src/users/users.module'
+import { Connection } from 'typeorm'
+import { User } from '../src/users/user.entity'
+import { UserRepository } from '../src/users/user.repository'
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
+  let userRepository: UserRepository
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule]
+      imports: [
+        AppModule,
+        UsersModule,
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'auth-service-db',
+          port: 3306,
+          username: 'root',
+          password: 'root',
+          database: 'auth-service-database',
+          entities: [User],
+          synchronize: true
+        })
+      ]
     }).compile()
 
     app = moduleFixture.createNestApplication()
+    userRepository = moduleFixture.get('UserRepository')
     await app.init()
+
+    await userRepository.query('DELETE FROM user')
+  })
+
+  afterEach(async () => {
+    await userRepository.query('DELETE FROM user')
+  })
+
+  afterAll(async () => {
+    const connection = app.get(Connection)
+    await connection.close()
+    await app.close()
   })
 
   describe('/POST auth/login', () => {
-    it('should return correct response if the login credentials are correct', () => {
+    it('should return correct response if the login credentials are correct', async () => {
+      await userRepository.save([
+        {
+          firstName: 'Apple',
+          lastName: 'Seed',
+          email: 'apple.seed@email.com',
+          password: 'password'
+        }
+      ])
+
       return request(app.getHttpServer())
         .post('/auth/login')
-        .send({ email: 'john', password: 'changeme' })
+        .send({ email: 'apple.seed@email.com', password: 'password' })
         .expect(201)
         .then(response => {
           expect(response.body).toHaveProperty('access_token')
         })
     })
 
-    it('should return 401 if the login credentials are incorrect', () => {
+    it('should return 401 if the login credentials are incorrect', async () => {
+      await userRepository.save([
+        {
+          firstName: 'Apple',
+          lastName: 'Seed',
+          email: 'apple.seed@email.com',
+          password: 'password'
+        }
+      ])
+
       return request(app.getHttpServer())
         .post('/auth/login')
-        .send({ email: 'notexist@email.com', password: 'changeme' })
+        .send({ email: 'john.doe@email.com', password: 'password1' })
         .expect(401)
         .then(err => {
           expect(err.body).toEqual({
@@ -38,9 +88,5 @@ describe('AppController (e2e)', () => {
           })
         })
     })
-  })
-
-  afterAll(async () => {
-    await app.close()
   })
 })
